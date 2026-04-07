9405f5a411eb409422af1fcf6715d98319f08c44
rpfrossard1-ctrl <rpfrossard1@gmail.com>
2026-04-07 13:46:35 -0400

    Update get-entries.js

--- netlify/functions/get-entries.js
+++ netlify/functions/get-entries.js
@@ -2,23 +2,30 @@ const { getStore } = require("@netlify/blobs");
 
 exports.handler = async () => {
   try {
-    const store = getStore("visitor-log");
-    const { blobs } = await store.list();
+    const store = getStore({
+      name: "sign-in-entries",
+      consistency: "strong"
+    });
 
-    const entries = await Promise.all(
-      blobs.map((b) => store.getJSON(b.key))
-    );
+    const entries = [];
+
+    for await (const entry of store.values()) {
+      entries.push(entry);
+    }
 
     return {
       statusCode: 200,
-      body: JSON.stringify(entries.filter(Boolean)),
+      body: JSON.stringify(entries),
+      headers: {
+        "Content-Type": "application/json"
+      }
     };
   } catch (err) {
     console.error("Fetch error:", err);
+
     return {
       statusCode: 500,
-      body: JSON.stringify([]),
+      body: JSON.stringify({ error: err.message })
     };
   }
 };
-``
