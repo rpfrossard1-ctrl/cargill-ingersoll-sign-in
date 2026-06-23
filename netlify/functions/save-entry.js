6a0f957c71a9350620ce888c2abe2136c7e44d1b
rpfrossard1-ctrl <rpfrossard1@gmail.com>
2026-06-23 10:40:39 -0400

    Update save-entry.js

--- netlify/functions/save-entry.js
+++ netlify/functions/save-entry.js
@@ -7,7 +7,24 @@ exports.handler = async (event) => {
     }
 
     
-const store = getStore("sign-in-entries")
+
+const { getStore } = require("@netlify/blobs");
+
+exports.handler = async () => {
+  const store = getStore({
+    name: "sign-in-entries",
+    siteID: process.env.NETLIFY_BLOBS_SITE_ID,
+    token: process.env.NETLIFY_BLOBS_TOKEN
+  });
+
+  await store.setJSON("test-key", { ok: true });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ok: true })
+  };
+};
+
 ;
     const data = JSON.parse(event.body);
 
